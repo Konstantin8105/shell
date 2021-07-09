4e4bad1733ae174d7467400ea1a7eea8c9f9fe42
Konstantin8105 <Konstantin8105@gmail.com>
2021-07-09 23:41:03 +0300

    remove unused comments

--- base_test.go
+++ base_test.go
@@ -1,6 +1,10 @@
 package shell
 
-// main - transpiled function from  GOPATH/src/github.com/Konstantin8105/shell/c-src/shell/eshell.c:1744
+import (
+	"fmt"
+	"os"
+)
+
 func Example() {
 
 	model := Model{
@@ -30,167 +34,14 @@ func Example() {
 			{N: 1, Forces: [3]float64{0, 11.899e6, 0}},
 		},
 	}
-
-	_ = model
-
-	// 	var i int
-	// 	n_m = 1
-
-	// 	n_n = 3
-
-	// 	n_e = 2
-
-	// 	n_d = 3
-
-	// n_f = 1
-
-	// 	m_E1 = make([]float64, n_m)
-	//
-	// 	m_E2 = make([]float64, n_m)
-	//
-	// 	m_G = make([]float64, n_m)
-	//
-	// 	m_nu1 = make([]float64, n_m)
-	//
-	// 	m_nu2 = make([]float64, n_m)
-	//
-	// 	m_q = make([]float64, n_m)
-	//
-	// 	m_vp = make([]float64, n_m)
-	//
-	// 	m_t = make([]float64, n_m)
-
-	// 	n_x = make([]float64, n_n)
-	//
-	// 	n_y = make([]float64, n_n)
-
-	// 	e_n1 = make([]int, n_e)
-	//
-	// 	e_n2 = make([]int, n_e)
-	//
-	// 	e_mat = make([]int, n_e)
-	//
-	// 	e_t = make([]float64, n_e)
-
-	// 	d_n = make([]int, n_d)
-	//
-	// 	d_dir = make([]int, n_d)
-	//
-	// 	d_val = make([]float64, n_d)
-
-	// 	f_n = make([]int, n_f)
-	//
-	// 	f_dir = make([]int, n_f)
-	//
-	// 	f_val = make([]float64, n_f)
-
-	// en_num = make([]int,n_n)
-
-	//en_frm = make([]int,n_n)
-
-	// 	m_E1[0], m_E2[0], m_G[0], m_nu1[0], m_nu2[0], m_q[0], m_vp[0], m_t[0] = 20e9, 0, 0, 0.2, 0, 25000, 1000, 0
-	//
-	// 	for i = 0; i < n_m; i++ {
-	//
-	// 		if m_E1[i] == m_E2[i] || m_E2[i] <= 0 {
-	//
-	// 			m_E2[i] = m_E1[i]
-	// 			m_nu2[i] = m_nu1[i]
-	// 			if m_G[i] <= 0 {
-	// 				m_G[i] = m_E1[i] / (2 * (1 + m_nu1[i]))
-	//
-	// 			}
-	// 		}
-	// 	}
-
-	// 	w_top, w_bot, w_val, w_min, w_max = 0, 0, 0, 0, 0
-
 	model.check_elem_data()
-
-	// 	fail_type = 1
-	//
-	// 	n_fail = 2
-	//
-	// 	fail_data = make([]float64, n_fail)
-	//
-	// 	fail_data[0] = 20e6
-	// 	fail_data[1] = 1e6
-	//
-	// 	n_r_inp = 1
-	//
-	// 	rand_type = make([]int,n_r_inp)
-	//
-	// 	rand_pos = make([]int,n_r_inp)
-	//
-	// 	rand_indx = make([]int,n_r_inp)
-	//
-	// 	rand_type[0], rand_pos[0], rand_indx[0] = 4, 0, 0
-
-	// n_r_opt = 0
-
-	// 	argc := int(len(os.Args))
-	// 	argv := []string{}
-	// 	for _, argvSingle := range os.Args {
-	// 		argv = append(argv, string(argvSingle))
-	// 	}
-	// 	defer noarch.AtexitRun()
-	// main() routine for standalone program only.
-	// var stat int
-	// os.Stdout = noarch.Stdout
-	// 	cmd_param(argc, argv)
-	// read_input_data() //noarch.Stdin)
-	//stat +=
-	// model.alloc_solver_data()
-	// stat += optim_replace_data(opt_data)
-	// 	if write_only == 1 {
-	// write_input_data() //noarch.Stdout)
-	// 		return
-	// 	}
-	// 	if solution_only == 1 {
-	//stat +=
 	K, F, u := model.get_matrix()
-	// stat += generate_water_load_x()
-	//stat +=
 	model.get_loads_and_supports(K, F, u)
-
-	// 	fmt.Printf("K = %.2e\n", mat.Formatted(K, mat.Prefix("    "), mat.Squeeze()))
-	// 	fmt.Println("FF", F)
-	//stat =
-	// n_n := len(model.Points)
-
-	// err :
-	_ = u.Solve(K, F)
-
-	// 	fmt.Println(">>>>>>>>>>>>>>> ", err)
-	//
-	// 	fmt.Println("uu ", u)
-
-	// 	femEqsCGwJ((&K), (&F), (&u), 1e-09, 6*3*n_n)
-	// 	}
-	// 	if n_r_inp > 0 && random_only == 1 {
-	// 		if solution_only != 0 {
-	// 			print_result(noarch.Stdout)
-	// 		}
-	// 		generate_rand_input_file(noarch.Stdout)
-	// 		generate_rand_out_file(noarch.Stdout)
-	// 	} else {
-	// 	if solution_only == 1 {
-	model.print_result(u) //noarch.Stdout)
-	// 	}
-	// 	}
-	// 	if solution_only == 1 {
-	// 	if fail_test() != 0 {
-	// 		fmt.Fprintf(os.Stdout, string("# Structure FAILED\n"))
-	// 	}
-	// 	}
-	// 	if price_only == 1 {
-	// 		if solution_only == 1 {
-	// 	fmt.Fprintf(os.Stdout, string("# Price is %f\n"), compute_price())
-	// 		} else {
-	// 			fmt.Fprintf(noarch.Stdout, string("%e\n"), compute_price())
-	// 		}
-	// 	}
-	// return
+	if err := u.Solve(K, F); err != nil {
+		fmt.Fprintf(os.Stdout, "%v", err)
+		return
+	}
+	model.print_result(u)
 
 	// Output:
 	// #  X     Y        w            u           angle            N1          N2           M1          M2          Q
